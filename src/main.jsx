fbdf9d1e7a836d4401754a98f0eb90665c04d86d
Sdtaylor87 <Stephen@mi-serv.co.uk>
2025-04-24 01:03:19 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return null
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
